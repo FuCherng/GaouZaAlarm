d97d8dc24bd2b622b5df8772822106d3d1670e26
shellyHBG <k820720k@gmail.com>
2014-12-06 20:03:05 +0800

    hello, 1. read image file from server 2. show in ImageView

--- httptest/gen/com/example/httptest/R.java
+++ httptest/gen/com/example/httptest/R.java
@@ -25,10 +25,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Btn_login=0x7f080002;
-        public static final int action_settings=0x7f080003;
-        public static final int user_name=0x7f080000;
-        public static final int user_pwd=0x7f080001;
+        public static final int Btn_login=0x7f080000;
+        public static final int ProBar_login=0x7f080002;
+        public static final int action_settings=0x7f080004;
+        public static final int iv01=0x7f080003;
+        public static final int txtTaskResult=0x7f080001;
     }
     public static final class layout {
         public static final int activity_http_test=0x7f030000;
--- httptest/src/com/example/httptest/HttpTest.java
+++ httptest/src/com/example/httptest/HttpTest.java
@@ -1,37 +1,175 @@
 package com.example.httptest;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
 import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.StrictMode;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class HttpTest extends Activity {
 	
-	Handler handler = new Handler();
-	FTPClient client;
+	Button Login;
+	private ProgressBar login_probar;
+	private TextView txtResult;
+	private String url = "http://140.113.207.184:8080/pictures/pic_001.png";
+	//"http://140.113.207.184:8080/download/?filepath=E:/GaouZaAlarm/python_shelly_server_db/pictures/pic_001.png"; 
+	//"http://140.113.207.184:8080/pictures/pic_001.png";
+	Bitmap rbm;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_http_test);
-        /*if (android.os.Build.VERSION.SDK_INT > 9) {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-        }*/        
-        new Thread(runnable).start(); 
-        //handler.post(runnable);
+        
+        Login = (Button)findViewById(R.id.Btn_login);
+        Login.setOnClickListener(login_btn);
+        
+        login_probar = (ProgressBar)findViewById(R.id.ProBar_login);
+        
+        txtResult = (TextView) findViewById(R.id.txtTaskResult);    
     }
     
-    Runnable runnable = new Runnable(){
+    public OnClickListener login_btn = new OnClickListener() {
+		public void onClick(View v) {			
+			//new LoadingDataAsyncTask().execute(100);
+			login_probar.setVisibility(View.VISIBLE);
+			new LoadDataFromServer().execute(url);			
+		}
+	};
+	
+	private String getData(String url){
+		HttpGet request = new HttpGet(url);
+		String result = "Not 200";
+		HttpClient client = new DefaultHttpClient();
+		try {
+			HttpResponse response = client.execute(request);
+			if (response.getStatusLine().getStatusCode() == 200) {
+				//result = EntityUtils.toString(response.getEntity());
+				result = "Get the file.";
+				
+				/*Intent it = new Intent(Intent.ACTION_VIEW);
+				Uri uri = Uri.parse(url);
+				it.setDataAndType(uri, "image/*");
+				startActivity(it); // 直接撥放音檔*/
+				
+				/*Uri uri = Uri.parse("http://140.113.207.184:8080/download/?filepath=E:/GaouZaAlarm/python_shelly_server_db/pictures/pic_0011.png");
+				Intent it = new Intent(Intent.ACTION_VIEW, uri);
+				startActivity(it); // 由 internet下載*/
+								
+				//Toast.makeText(HttpTest.this, result, Toast.LENGTH_LONG).show();				
+			}
+		} catch (Exception e) {  		
+			//return e.toString();
+			result = e.toString();
+		} finally {
+			client.getConnectionManager().shutdown();
+		}
+		return result;
+	}
+
+	private void showData(String result){
+		txtResult.setText(result);
+		login_probar.setVisibility(View.INVISIBLE);
+		// show ImageView
+		ImageView imageView = (ImageView)findViewById(R.id.iv01);
+		imageView.setImageBitmap(rbm);
+	}
+	
+	private Bitmap getImageBitmap(String url) { 
+        Bitmap bm = null; 
+        try { 
+            URL aURL = new URL(url); 
+            URLConnection conn = aURL.openConnection(); 
+            conn.connect(); 
+            InputStream is = conn.getInputStream(); 
+            BufferedInputStream bis = new BufferedInputStream(is); 
+            bm = BitmapFactory.decodeStream(bis); 
+            bis.close(); 
+            is.close(); 
+       } catch (IOException e) { 
+    	   Log.e("Error getting bitmap", e.toString());
+       } 
+       return bm; 
+    } 
+	
+	class LoadDataFromServer extends AsyncTask<String, Integer, String>{
+		@Override
+		protected String doInBackground(String... params) {
+			//String res = getData(params[0]);
+			rbm = getImageBitmap(params[0]);
+			return "Get The File.";
+		}
+		@Override
+		protected void onProgressUpdate(Integer... progress) {
+			super.onProgressUpdate(progress);
+		}
+		@Override
+		protected void onPostExecute(String result) { // background執行完後才會call
+			//super.onPostExecute(result);
+			//txtResult.setText(result);
+			showData(result);
+		}
+	}
+	
+	class LoadingDataAsyncTask extends AsyncTask<Integer, Integer, String>{
+
+		@Override
+		protected String doInBackground(Integer... params) { // 一定必須覆寫的方法，把會花時間執行的內容放在這裡
+			//getData(); // 這裡不能和UI有任何互動--取資料就好><
+			for(int i=0;i<=100;i++){
+				login_probar.setProgress(i);
+				publishProgress(i,5);
+				try {
+					Thread.sleep(params[0]);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			return "Loading done !";
+		}
+
+		@Override
+		protected void onProgressUpdate(Integer... progress) { // 這個方法會取得一個數值，
+			//可以用來計算目前執行的進度，通常用來改變進度列(ProgressBar)的顯示。 
+			//super.onProgressUpdate(progress);
+			//UpdateProgress(values);
+			txtResult.setText(String.valueOf(progress[0]));
+		}
+		
+		@Override
+		protected void onPostExecute(String result) { // background執行完後才會call
+			//super.onPostExecute(result);
+			showData(result); // 用來把取得的資料送給UI來顯示
+		}
+		
+		@Override
+		protected void onPreExecute() { // 在背景執行之前要做什麼事都是寫在這裡。
+			super.onPreExecute();
+		}
+
+	}
+    /*Runnable runnable = new Runnable(){
         @Override
         public void run() {
             // TODO: http request.
@@ -54,9 +192,9 @@ public class HttpTest extends Activity {
     		//Log.d("Get Response", result);
         }
         //handler.removeCallbacks(runnable);
-    };
+    };*/
     
-    @Override
+    /*@Override
     protected void onPause() {
         // TODO Auto-generated method stub
         super.onPause();
@@ -65,17 +203,17 @@ public class HttpTest extends Activity {
         }
         Thread.interrupted();
         runnable=null;
-    }
+    }*/
     
     
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.http_test, menu);
         return true;
-    }
+    }*/
 
-    @Override
+    /*@Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -85,5 +223,5 @@ public class HttpTest extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
